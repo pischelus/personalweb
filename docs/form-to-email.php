76068b576c385d3718fc54046dca75c102f1eaee
pischelus <pischelus@users.noreply.github.com>
2019-12-20 10:59:56 -0800

    test php

--- docs/form-to-email.php
+++ docs/form-to-email.php
@@ -1,34 +1,4 @@
 <?php
-	function IsInjected($str)
-	{
-		$injections = array('(\n+)',
-			   '(\r+)',
-			   '(\t+)',
-			   '(%0A+)',
-			   '(%0D+)',
-			   '(%08+)',
-			   '(%09+)'
-			   );
-				   
-		$inject = join('|', $injections);
-		$inject = "/$inject/i";
-		
-		if(preg_match($inject,$str))
-		{
-			return true;
-		}
-		else
-			{
-			return false;
-		}
-	}
-
-	if(IsInjected($visitor_email))
-	{
-		echo "Bad email value!";
-		exit;
-	}
-	
 	$to = "krdarmasaputra@gmail.com";
 	$name = $_POST['name'];
 	$visitor_email = $_POST['email'];
